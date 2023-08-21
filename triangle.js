d1dcd556c6eb3dc50b0ac7624aa7a4bd0e9472a2
MD RUPOM HASAN <rupom.hasan607299@gmail.com>
2023-08-22 01:25:13 +0600

    Area claculator

---
+++ triangle.js
@@ -0,0 +1,15 @@
+
+// document.getElementById('tringle-btn').addEventListener('click',function(){
+
+
+    // // base value
+    // const getBase  = document.getElementById('tringle-Base')
+    // const baseValue = parseFloat(getBase.value);
+    // console.log(baseValue);
+    // // height value
+    // const getHeight = document.getElementById('triangle-Height')
+    // const heightValue = parseFloat(getHeight.value)
+    // console.log(heightValue );
+    // console.log('click')
+    
+// })
